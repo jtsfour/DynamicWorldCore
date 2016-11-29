05bfb169eb3979fed236e5819400488a48661326
jtsfour <jtshelley4@gmail.com>
2016-11-28 18:35:08 -0600

    Still building dev env

---
+++ java/com/jtsfour/dwcore/DynamicWorldCore.java
@@ -0,0 +1,18 @@
+package com.jtsfour.dwcore;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = DynamicWorldCore.MODID, version = DynamicWorldCore.VERSION)
+public class DynamicWorldCore
+{
+    public static final String MODID = "dwcore";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+}
