f9d0a4d790169e175969282a7b911efa992ce86a
jtsfour <jtshelley4@gmail.com>
2017-03-07 21:15:37 -0600

    stuff

--- java/com/jtsfour/dwcore/client/render/block/TestTileEntitySpecialRenderer.java
+++ java/com/jtsfour/dwcore/client/render/block/TestTileEntitySpecialRenderer.java
@@ -6,7 +6,6 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.init.Items;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
@@ -21,80 +20,13 @@ public class TestTileEntitySpecialRenderer extends TileEntitySpecialRenderer {
 		    
 		    vb.begin(7, DefaultVertexFormats.POSITION_TEX);
 		    Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("dwcore:textures/blocks/dwcore_testtileentity_side_0.png"));
-		    //vb.normal(0.0F, 1.0F, 0.0F);
-		    /*
-		    vb.pos(1.0, 1.0, 0.0).tex(1.0, 1.0).endVertex();
-		    vb.pos(1.0, 1.0, 1.0).tex(0.0, 1.0).endVertex();
-		    vb.pos(0.0, 1.0, 1.0).tex(0.0, 0.0).endVertex();
-		    vb.pos(0.0, 1.0, 0.0).tex(1.0, 0.0).endVertex();
-		    */
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    /*
-		    vb.pos(1.0, 1.0, 1.0).tex(1.0, 1.0).endVertex();
-		    vb.pos(0.0, 1.0, 1.0).tex(0.0, 1.0).endVertex();
-		    vb.pos(0.0, 1.0, 0.0).tex(0.0, 0.0).endVertex();
-		    vb.pos(1.0, 1.0, 0.0).tex(1.0, 0.0).endVertex();
-		    */
+
 		    vb.pos(1.0, 1.0, 0.0).tex(1.0, 0.0).endVertex();
 		    vb.pos(0.0, 1.0, 0.0).tex(0.0, 0.0).endVertex();
 		    vb.pos(0.0, 1.0, 1.0).tex(0.0, 1.0).endVertex();
 		    vb.pos(1.0, 1.0, 1.0).tex(1.0, 1.0).endVertex();
-		    
-		    
-		    
-		    
-		    //GlStateManager.scale(0.2, 0.2, 0.2);
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    
-		    /*
-		    vb.pos(1.0, 0.0, 0.0).tex(0.0, 1.0).endVertex();
-		    vb.pos(0.0, 0.0, 0.0).tex(1.0, 1.0).endVertex();
-		    vb.pos(0.0, 0.5, 0.0).tex(1.0, 0.5).endVertex();
-		    vb.pos(1.0, 0.5, 0.0).tex(0.0, 0.5).endVertex();
-		    */
-		    /*
-		     * wr.addVertexWithUV(1.0, 0.0, 0.0, 0.0, 1.0); // Bottom left
-wr.addVertexWithUV(0.0, 0.0, 0.0, 1.0, 1.0); // Bottom right
-wr.addVertexWithUV(0.0, 0.5, 0.0, 1.0, 0.5); // Top right
-wr.addVertexWithUV(1.0, 0.5, 0.0, 0.0, 0.5); // Top left
-		     * 
-		     */
-		    //GlStateManager.rotate(-25, 0, 0, 1);
+		    GlStateManager.rotate(35F, 1F, 0F, 0F);
+		    GlStateManager.scale(0.5F, 0.5F, 1F);
 		    Tessellator.getInstance().draw();
 		    GlStateManager.popMatrix();
 	}
